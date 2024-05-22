14ef68b3ffc552994f32eab565b9bd5777496364
VladimirJS007 <kutovoyv794@gmail.com>
2024-05-22 10:54:57 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,34 @@
+// Number
+let UserAge  = 18;
+let Value1   = 1;
+let SomeVal  = 123;
+let zero_val = 0;
+let newValue = SomeVal + 10;
+
+// Strings
+let user_mail = "tempuser@mail.com";
+let user_age  = `User age: ${UserAge + 5}`
+let someTextWithMail = `Mail to: ${user_mail}`;
+
+// Display in Console
+console.log(Value1);
+console.log(newValue);
+console.log(user_mail);
+console.log(user_age);
+console.log(someTextWithMail);
+
+// NaN
+let NanVal = zero_val/zero_val;
+
+// NULL
+let NullVal = null;
+
+console.log(NanVal);
+console.log(NullVal);
+
+// Reuse variables
+Value1 = 50;
+console.log(Value1);
+
+user_mail = "temp2user@gmail.com";
+console.log(user_mail);
